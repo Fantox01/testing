ad30102948e4815f468097a907b066504a5a17fa
FantoX <105273285+Fantox01@users.noreply.github.com>
2023-02-12 13:25:21 +0530

    Create arch.sh

---
+++ arch.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+sudo pacman -Syu
+sudo pacman -S libwebp
+sudo pacman -S git
+sudo pacman -S nodejs
+sudo pacman -S ffmpeg
+sudo pacman -S wget
+sudo pacman -S npm
+sudo pacman -S imagemagick
+npm install --global yarn
+yarn add sharp
+sudo pacman -S curl
+curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo pacman -S nodejs
+yarn add sharp
+git clone https://github.com/Fantox01/Atlas-MD
+cd Atlas-MD
+yarn
+npm start
