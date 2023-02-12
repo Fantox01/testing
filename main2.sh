a92133f7dac06e2a15bd2c22d43f916a12c5ccb0
FantoX <105273285+Fantox01@users.noreply.github.com>
2023-02-12 13:51:34 +0530

    Create main2.sh

---
+++ main2.sh
@@ -0,0 +1,19 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install bash
+sudo apt-get install libwebp-dev
+sudo apt install git
+sudo apt install nodejs -y
+sudo apt install ffmpeg -y
+sudo apt install npm
+sudo apt install imagemagick
+git clone https://github.com/FantoX001/Atlas-MD
+npm install --global yarn
+yarn add sharp
+sudo apt install curl
+wget -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt-get install -y nodejs
+cd Miku-MD
+npm i
+npm i sharp
+npm start
